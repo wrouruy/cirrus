c496acaa7fe36e313da5f03ddb1a99e9319439ad
wrouruy <wroyruy@gmail.com>
2026-02-14 13:27:12 +0200

    some change readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# how to install & set
+1. clone cirrus: ``` https://github.com/wrouruy/cirrus.git && cd cirrus ```
+2. compile it: ``` gcc main.c -o cirrus ```
+3. make compiled file global(linux): ``` sudo ln -s "$(pwd)/cirrus" /usr/bin ```
---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/stat.h>
+
+char** global_argv;
+int global_argc;
+
+#include "utils.h"
+#include "conf-reader.h"
+#include "Lexer/lexer.h"
+#include "Parser/parser.h"
+#include "flags.h"
+
+int main(int argc, char* argv[])
+{
+    global_argv = argv;
+    global_argc = argc;
+
+    flag_t flags[] = {
+        { "--help", print_help    },
+        { "-h",     print_help    },
+
+        { "--version", print_version },
+        { "-v",        print_version },
+
+        { "-repo",  open_repo     },
+        { "-S",     build_asm     }
+    };
+
+    for(int i = 0; i < sizeof(flags) / sizeof(flag_t); i++)
+        if(include_str(flags[i].name, argv, argc)){
+            flags[i].func();
+            return 0;
+        }
+
+    return 0;
+}
